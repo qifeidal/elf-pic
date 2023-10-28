396ea0afba0753d8a49699db39e4194243cabbda
qifeidal <149225310+qifeidal@users.noreply.github.com>
2023-10-28 08:37:28 +0000

    Genie Wallpapers

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   site: 'https://cannonball.littlesticks.dev',
   name: 'Elf Wallpaper',
-  title: 'Elf Wallpaper | A free mobile wallpaper APP',
+  title: 'Genie Wallpapers | A free mobile wallpaper APP',
   description: 'This is a free mobile wallpaper APP',
 }
